09909c4ec54c5af24e65e99ef44c6e3659faef50
giova <gcad96@gmail.com>
2019-11-08 10:41:36 +0100

    aggiornamento

--- algoritmo.c
+++ algoritmo.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "cella.h"
+#include "celle.h"
 #include "allocazione.h"
 
 void trovaPercorso()
 {
-
+    celle c;
+    allocaCelle(&c);
+    stampaCelle(c);
 }
