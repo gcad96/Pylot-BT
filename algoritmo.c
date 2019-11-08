7fcab4fd3467d26e850d2ab2cb1c7f2284047926
giova <gcad96@gmail.com>
2019-11-08 10:36:49 +0100

    aggiornamento

--- algoritmo.c
+++ algoritmo.c
@@ -1,12 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "utility.h"
-#include "coordinata.h"
-#include "topologia.h"
-#include "cella.h"
-#include "celle.h"
-#include "gruppo.h"
-#include "testa.h"
 #include "allocazione.h"
 
 void trovaPercorso()
