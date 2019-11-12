a2dba5daf6430dba17189f1ddfb698439058eba7
giova <gcad96@gmail.com>
2019-11-12 10:29:51 +0100

    aggiunts funzione path

--- algoritmo.c
+++ algoritmo.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "utility.h"
+#include "coordinata.h"
 #include "cella.h"
 #include "celle.h"
 #include "testa.h"
 #include "teste.h"
 #include "allocazione.h"
+#include "algoritmo.h"
+
+#define PIUVICINE 4
 
 void definisciNumeroTeste(int* n);
 void generaInsiemiDiCelleContigue(celle c);
 void definisciNumeroMaxCelle(int* n);
+void path(celle c, int card);
+void pathRic(cella u, celle c, int n, int card, int *coll);
 
 void trovaPercorso()
 {
@@ -50,7 +56,7 @@ void generaInsiemiDiCelleContigue(celle c)
     int i, j, k;
     for(i=1; i<=card; i++)
     {
-
+        path(c, card);
     }
 }
 
@@ -69,5 +75,58 @@ void definisciNumeroMaxCelle(int* n)
     {
         fprintf(stderr, "Errore nel file di configurazione.\n");
         return;
+    }
+}
+
+void path(celle c, int card)
+{
+    cella* insieme = getInsieme(c);
+    int dim = getDim(c);
+
+    int i;
+    for(i=0; i<dim; i++)
+    {
+        cella u = insieme[i];
+        int n = 0;
+        int* coll = malloc(card*sizeof(int));
+
+        pathRic(u, c, n, card, coll);
+
+        free(coll);
+    }
+}
+
+void pathRic(cella u, celle c, int n, int card, int *coll)
+{
+    coll[n++] = getId(u);
+    if(n!=card)
+    {
+        cella* vicine = malloc(PIUVICINE*sizeof(cella)); getNCellePiuVicina(c, u, PIUVICINE, vicine);
+        int j;
+        for(j=0; j<PIUVICINE; j++)
+        {
+            cella s = vicine[j];
+            int k;
+            int giaCons = 0;
+            for(k=0; k<n; k++)
+            {
+                if(coll[k]==getId(s))
+                {
+                    giaCons = 1;
+                    break;
+                }
+            }
+            if(!giaCons)    pathRic(s, c, n, card, coll);
+        }
+        free(vicine);
+    }
+    else
+    {
+        int i;
+        for(i=0; i<n; i++)
+        {
+            printf("%d ", coll[i]);
+        }
+        printf("\n");
     }
 }
